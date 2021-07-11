d43486a902bffd218d312e9586c69d44e36d5286
Syrkin Vladimir <tempresdisk@yandex.ru>
2021-07-11 17:00:31 +0400

    update filter ingredient name, TODO uppercase, lowercase etc.

--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -1,4 +1,3 @@
-from rest_framework import filters
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -14,9 +13,14 @@ class TagViewSet(ReadOnlyModelViewSet):
 
 
 class IngredientViewSet(ReadOnlyModelViewSet):
-    queryset = models.Ingredient.objects.all()
+    #queryset = models.Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
     permission_classes = [AllowAny]
     pagination_class = None
-    search_fields = ['^name']
-    filter_backends = (filters.SearchFilter,)
+    #filterset_fields = ['name']
+    #search_fields = ['^name']
+
+    def get_queryset(self):
+        ingredient_name = self.request.query_params['name']
+        ingredients = models.Ingredient.objects.filter(name__startswith=ingredient_name)
+        return ingredients
