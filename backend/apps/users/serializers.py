84f323ccfa6244919a3c70634efb08d19b7b62b4
Syrkin Vladimir <tempresdisk@yandex.ru>
2021-07-18 16:10:06 +0400

    get_user_model in users/serializers

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import CustomUser
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_subscribed', 'password')
-        model = CustomUser
+        model = User
         extra_kwargs = {
                 'password': {'write_only': True}
         }
