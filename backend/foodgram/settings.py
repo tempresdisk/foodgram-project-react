93210a80dce0d80376d21df37e7f54c3e754875b
Syrkin Vladimir <tempresdisk@yandex.ru>
2021-07-19 11:37:44 +0400

    update settings with cors_urls_regex

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,6 +13,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', 'web']
 
 CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_WHITELIST = []  # добавить IP, доменное имя своей ВМ
+CORS_URLS_REGEX = r'^/api/.*$'
 
 
 INSTALLED_APPS = [
