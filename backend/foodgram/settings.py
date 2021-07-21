75f5397a9fb8f0add42a02e14d8dbfd6502707e9
Syrkin Vladimir <tempresdisk@yandex.ru>
2021-07-21 18:57:32 +0400

    workflow deploy 1st attempt ready

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,12 +8,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', 'web']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', 'web', '178.154.252.191']
 
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = []  # добавить IP, доменное имя своей ВМ ?
+if DEBUG:
+    CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
 CORS_URLS_REGEX = r'^/api/.*$'
 
 
@@ -67,8 +68,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
