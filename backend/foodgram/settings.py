2fe0d50032087468d637ab41356ee8af5885ccea
Syrkin Vladimir <tempresdisk@yandex.ru>
2021-07-24 12:26:32 +0400

    cors strict testing

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', 'web', '178.154.252.191']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', 'web']
 
 CORS_ORIGIN_ALLOW_ALL = False
 
